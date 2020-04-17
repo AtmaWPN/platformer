6e9c6a9d4808a0ac1dce0088bac49edc63ad7e92
AtmaWPN <63207425+AtmaWPN@users.noreply.github.com>
2020-04-17 07:54:46 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Create the canvas
-var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
-canvas.width = 912;
-canvas.height = 676;
-document.body.appendChild(canvas);
