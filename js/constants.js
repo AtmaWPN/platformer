0c95d3898fa29651732692fd175e513f245aa81b
AtmaWPN <63207425+AtmaWPN@users.noreply.github.com>
2020-04-17 07:54:33 -0500

    add constants and levels js files

---
+++ js/constants.js
@@ -0,0 +1,31 @@
+//constants
+var grid = 32;
+var STAGE_WIDTH = 900,
+	STAGE_HEIGHT = 600,
+	TIME_PER_FRAME = 33, //this equates to 30 fps
+	GAME_FONTS = "bold 20px sans-serif";
+	
+var COUNTER_X = 100; 
+var COUNTER_Y = 100;
+
+var black = 'rgb(0 0 0)';
+var	grey = 'rgb(128 128 128)';
+var	white = 'rgb(255 255 255)';
+var	red = 'rgb(255 0 0)';
+var	green = 'rgb(0 255 0)';
+var	blue = 'rgb(0 0 255)';
+var	yellow = 'rgb(255 255 0)';
+var	lblue = 'rgb(0 255 255)';
+var	magenta = 'rgb(255 0 255)';
+var greyOverlay = 'rgba(0, 0, 0, 0.5)';
+
+var playerHeight = 58;
+var crouchHeight = 32;
+var playerWidth = 32;
+var deathFrames = 10;
+
+var speed = 8;
+var jumpHeight = -17;
+var gravityConst = 1.1;
+var coyoteFrames = 5;
+
